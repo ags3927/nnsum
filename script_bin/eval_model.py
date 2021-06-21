bab927fccaa6fb8f76da35e15bb1213c55ae2b04
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2021-06-21 17:59:29 +0600

    :sparkles: added sorted summaries and bangla rouge score function

--- script_bin/eval_model.py
+++ script_bin/eval_model.py
@@ -9,6 +9,23 @@ import pandas as pd
 import rouge_papier
 from multiprocessing import cpu_count
 
+from typing import *
+from rouge import Rouge
+rouge = Rouge()
+
+
+def eval_rouge_bangla(can: str, ref: str) -> Tuple[float, float, float]:
+    """
+        Use Rouge library to naively calculate rouge score for Bangla
+        :param can: Hypothesis/candidate summary
+        :param ref: Ground Truth summary
+        :return:
+    """
+    if can == '' or ref == '':
+        return 0.0, 0.0, 0.0
+    scores = rouge.get_scores(can, ref)
+    return scores[0]['rouge-1']['f'], scores[0]['rouge-2']['f'], scores[0]['rouge-l']['f']
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -54,6 +71,11 @@ def main():
     ids = []
     path_data = []
     model.eval()
+    
+    rouge_1_scores = []
+    rouge_2_scores = []
+    rouge_l_scores = []
+    
     with rouge_papier.util.TempFileManager() as manager:
         with torch.no_grad():
             for step, batch in enumerate(loader, 1):
@@ -61,15 +83,56 @@ def main():
                 print("generating summaries {} / {} ...".format(
                         step, len(loader)),
                     end="\r" if step < len(loader) else "\n", flush=True)
-                texts, doc_ids = model.predict(batch, max_length=args.summary_length)
+                texts, sent_ids, doc_ids = model.predict(batch, return_indices=True, max_length=args.summary_length)
+                
+                
+                sorted_texts = []
+                
+                for text_arr, sent_id_arr in zip(texts, sent_ids):
+                    summary = []
+                    for sent, sent_id in zip(text_arr, sent_id_arr):
+                        summary.append({
+                            "sentence": sent,
+                            "sentence_id": sent_id
+                        })
+                    summary.sort(key=lambda x: x["sentence_id"])    
+                    sorted_texts.append(summary)
                 
                 for idx, doc_id in enumerate(doc_ids):
                     with open(gen_summary_path + '/' + str(doc_id) + '.json', 'w', encoding='utf8') as f:
-                        original_json.dump({
-                            'id': doc_id,
-                            'summary': texts[idx]
-                        }, f, ensure_ascii=False)
+                        summary_sentences = [sorted_texts[idx][sentence_idx]["sentence"] for sentence_idx in range(len(sorted_texts[idx]))]
+                        summary_text = "\n".join(summary_sentences)
+                        f1 = open(str(args.refs) + '/' + str(doc_id) + ".spl", 'r')
+                        ref_text = f1.read()
+                        rouge_1, rouge_2, rouge_l = eval_rouge_bangla(summary_text, ref_text)
                         
+                        summary_obj = {
+                            'id': doc_id,
+                            'summary': summary_text,
+                            'ground_truth': ref_text,
+                            'rouge-1': rouge_1,
+                            'rouge-2': rouge_2,
+                            'rouge-l': rouge_l
+                        }
+                        rouge_1_scores.append(rouge_1)
+                        rouge_2_scores.append(rouge_2)
+                        rouge_l_scores.append(rouge_l)
+                        
+                        original_json.dump(summary_obj, f, ensure_ascii=False)
+                        
+                
+                # for idx, doc_id in enumerate(doc_ids):
+                #     summary_obj = {
+                #             'id': doc_id,
+                #             'summary': sorted_texts[idx]
+                #     }
+                #     original_json.dump(summary_obj, f, ensure_ascii=False)
+                #     summary_sentences = [sorted_text[idx][sentence_idx]["sentence"] for sentence_idx in range(len(sorted_text[idx]))]
+                #     summary_text = "\n".join(summary_sentences)
+                #     f = open(args.refs + doc_id + ".spl")
+                #     ref_text = f.read()
+                #     rouge_1, rouge_2, rouge_l = eval_rouge_bangla(summary_text, ref_text)
+                
                 
                     
                 for text, ref_paths in zip(texts, batch.reference_paths):
@@ -96,8 +159,24 @@ def main():
                                      for id, record in zip(ids, records)},
                        "average": df[-1:].to_dict("records")[0]}
             args.results.parent.mkdir(parents=True, exist_ok=True)
-            with args.results.open("w") as fp:
-                fp.write(json.dumps(results))
+            # with args.results.open("w") as fp:
+            #     fp.write(json.dumps(results))
+
+        num_of_test_data = len(loader)*args.batch_size
+        avg_rouge_1 = (sum(rouge_1_scores)/num_of_test_data)
+        avg_rouge_2 = (sum(rouge_2_scores)/num_of_test_data)
+        avg_rouge_l = (sum(rouge_l_scores)/num_of_test_data)
+        results = {
+            "Rouge-1": avg_rouge_1,
+            "Rouge-2": avg_rouge_2,
+            "Rouge-L": avg_rouge_l
+        }
+        with args.results.open("w") as fp:
+            fp.write(json.dumps(results))
+        
+        print("Average rouge-1 score = " + str(avg_rouge_1) + "\n")
+        print("Average rouge-2 score = " + str(avg_rouge_2) + "\n")
+        print("Average rouge-l score = " + str(avg_rouge_l) + "\n")
         
 if __name__ == "__main__":
     main()
